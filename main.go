8b514ecf6179a2fb0e3a71157d4cbfe415873660
Colton <coltonjsooth@yahoo.com>
2026-03-04 20:27:41 -0800

    Day 3: Refactor journal app and seperate functions

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 	"time"
 )
 
@@ -11,34 +12,57 @@ func main() {
 	wd, _ := os.Getwd()
 	fmt.Println("Working directory:", wd)
 
+	noteText, err := promptForNote()
+	if err != nil {
+		fmt.Println("error reading note:", err)
+		return
+	}
+
+	entry := formatEntry(noteText)
+
+	err = appendToJournal(entry)
+	if err != nil {
+		fmt.Println("error writing journal:", err)
+		return
+	}
+
+	fmt.Println("Entry saved successfully.")
+}
+
+// asks user fir note
+func promptForNote() (string, error) {
 	fmt.Println("enter today's note: ")
 
 	reader := bufio.NewReader(os.Stdin)
 
-	note, err := reader.ReadString('\n')
+	noteText, err := reader.ReadString('\n')
 	if err != nil {
-		fmt.Println("error reading input:", err)
-		return
+		return "", err
 	}
 
-	// add time to note
-	currentTime := time.Now()
-	formattedTime := currentTime.Format("2006-01-02 15:04:05")
-	entry := formattedTime + " - " + note
+	noteText = strings.TrimSpace(noteText)
+	return noteText, nil
+}
 
-	// open or create journal.txt for appending
+// adds timestamp to note
+func formatEntry(noteText string) string {
+	formattedTime := time.Now().Format("2006-01-02 15:04:05")
+	return formattedTime + " - " + noteText + "\n"
+
+}
+
+// append entry to journal.txt
+func appendToJournal(entry string) error {
 	file, err := os.OpenFile("journal.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 	if err != nil {
-		fmt.Println("error opening file:", err)
-		return
+		return err
 	}
 	defer file.Close()
+
 	_, err = file.WriteString(entry)
 	if err != nil {
-		fmt.Println("error writing to file:", err)
-		return
+		return err
 	}
 
-	fmt.Println("You Wrote:")
-	fmt.Println(note)
+	return nil
 }
